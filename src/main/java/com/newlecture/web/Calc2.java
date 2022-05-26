a21732776d2cc8bb3d3e53de748759f2acbba5f5
HongDongHwan <hongdh94@gmail.com>
2022-05-26 16:35:39 +0900

    ADD Servlet Study 5/26 Servlet
    29 - Cookie를 이용해 상태값 유지하기
    서버 쪽 저장소 : Application, Session
    클라이언트 쪽 저장소 : Cookie
    30 - Cookie의 path 옵션
    31 - Cookie의 maxAge 옵션 (만료날짜지정)
    32 - Application/Session/Cookie 차이점 정리
    Applicaion
    사용범위 : 전역 범위에서 사용하는 저장 공간
    생명주기 : WAS가 시작해서 종료될 때 까지
    저장위치 : WAS 서버의 메모리
    Session
    사용범위 : 세션 범위에서 사용하는 저장 공간
    생명주기 : 세션이 시작해서 종료할 때 까지
    저장위치 : WAS 서버의 메모리
    Cookie
    사용범위 : Web Browser별 지정한 path 범주 공간
    생명주기 : Browser에 전달한 시간부터 만료시간까지
    저장위치 : Web Browser의 메모리 또는 파일
    33 - 서버에서 페이지 전환해주기(redirection)
    34 - 동적인 페이지(서버 페이지)의 필요성
    35 - 동적인 페이지 서블릿으로 직접 만들기
    36 - 계산기 서블릿 완성하기
    37 - 쿠키 삭제하기 ( null값과 setMaxAge 활용 !)
    38 - GET과 POST에 특화된 서비스 함수
    39 - 계산기 프로그램 하나의 서블릿으로 합치기

--- src/main/java/com/newlecture/web/Calc2.java
+++ src/main/java/com/newlecture/web/Calc2.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,6 +21,9 @@ public class Calc2 extends HttpServlet {
 		
 		ServletContext application = request.getServletContext();
 		HttpSession session = request.getSession();
+		Cookie[] cookies = request.getCookies();
+		
+		
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
 		
@@ -34,10 +38,26 @@ public class Calc2 extends HttpServlet {
 		if(op.equals("=")) {
 			
 			// int x = (Integer)application.getAttribute("value");
-			int x = (Integer)session.getAttribute("value");
+			// int x = (Integer)session.getAttribute("value");
+			int x = 0;
+			for(Cookie c : cookies) 
+				if(c.getName().equals("value")) {
+					x = Integer.parseInt(c.getValue());
+					break;
+				}
+			
+			
 			int y = v;
 			// String operator = (String)application.getAttribute("op");
-			String operator = (String)session.getAttribute("op");
+			// String operator = (String)session.getAttribute("op");
+			
+			String operator = "";
+			for(Cookie c : cookies) 
+				if(c.getName().equals("op")) {
+					operator = c.getValue();
+					break;
+				}
+			
 			int result = 0;
 			
 			if(operator.equals("+"))
@@ -51,8 +71,20 @@ public class Calc2 extends HttpServlet {
 		else {
 			// application.setAttribute("value", v);
 			// application.setAttribute("op", op);
-			session.setAttribute("value", v);
-			session.setAttribute("op", op);
+			
+			// session.setAttribute("value", v);
+			// session.setAttribute("op", op);
+			
+			Cookie valueCookie = new Cookie("value", String.valueOf(v));
+			Cookie opCookie = new Cookie("op", op);
+			valueCookie.setPath("/calc2");
+			valueCookie.setMaxAge(24*60*60); // 초 단위
+			opCookie.setPath("/calc2");
+			
+			response.addCookie(valueCookie);
+			response.addCookie(opCookie);
+			
+			response.sendRedirect("calc2.html");
 		}
 		
 		
